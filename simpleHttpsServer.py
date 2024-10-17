d8393236f2ca8ecfbcc217817ba5bafba885a45a
aka CptSpiff <zaphoxx@gmail.com>
2024-10-17 11:03:57 +0200

    Update simpleHttpsServer.py

--- simpleHttpsServer.py
+++ simpleHttpsServer.py
@@ -5,6 +5,8 @@ import sys
 
 LHOST = "0.0.0.0"
 LPORT = 443
+CERT = '/path/to/certfile'
+PRIVKEY = '/path/to/privkeyfile'
 
 if len(sys.argv) == 3:
   LHOST = sys.argv[1]
@@ -12,9 +14,9 @@ if len(sys.argv) == 3:
 
 try:
     httpd = HTTPServer((LHOST, LPORT), SimpleHTTPRequestHandler)
-    httpd.socket = ssl.wrap_socket (httpd.socket, 
-        keyfile="key.pem", 
-        certfile='certificate.pem', server_side=True)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(certfile=CERT,keyfile=PRIVKEY)
+    httpd.socket = context.wrap_socket(httpd.socket, server_side=True) 
     print(F"*** https-server running and listening on {LHOST} - {LPORT} ***")
     httpd.serve_forever()
 except Exception as https_error:
